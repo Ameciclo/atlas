4a836734e18a3afa778c3e13445bb265f05b0ced
Daniel Valença <dvalenca@gmail.com>
2025-10-29 11:47:00 -0300

    removendo traffic-calls, estranho estava aqui

--- apps/traffic-calls/src/app.ts
+++
@@ -1,9 +0,0 @@
-import createApp from "./lib/create-app.js";
-import healthRoutes from "./routes/health.js";
-import exampleRoutes from "./routes/example/example.index.js";
-
-const app = createApp()
-	.route("/", healthRoutes)
-	.route("/v1/", exampleRoutes);
-
-export default app;
